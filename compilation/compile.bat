312303d37a0359b72dbbe15df83acc4c906311bc
YALdysse <yaroslav_lytvynov@yahoo.com>
2026-02-20 20:29:39 +0200

    Initial commit

---
+++ compilation/compile.bat
@@ -0,0 +1,2 @@
+..\utils\gxt\gxt.exe -i ".\source_txt\ukrainian-utf-8-bom.txt" -o ".\output_gxt\ukrainian.gxt" -t ".\tables\iii_table_pc_ukr_andrulko_hd-fonts.txt"
+pause
---
+++ decompilation/decomp.bat
@@ -0,0 +1,2 @@
+..\utils\gxt\gxt.exe -i ".\source_gxt\ukrainian_andrulko_dimzet.gxt" -o ".\output_txt\ukrainian.txt" -t ".\table\iii_table_pc_ukr_andrulko_dimzet.txt
+pause
